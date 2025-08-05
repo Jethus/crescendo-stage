1a16db4ac6f03f5e8adb14ecf7e54cccfddfcc4e
Jethus <johtade@rogers.com>
2025-08-04 21:49:44 -0400

    Most of the landing page done

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,10 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { glob, file } from "astro/loaders";
 
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   // Type-check frontmatter using a schema
-  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/data/blog" }),
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/blog" }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
@@ -18,12 +18,13 @@ const blog = defineCollection({
 
 const services = defineCollection({
   // Load Markdown files in the `src/content/services/` directory.
-
+  loader: file("./src/content/services.json"),
   schema: ({ image }) =>
     z.object({
       heading: z.string(),
       image: image(),
-      order: z.number().optional(),
+      description: z.string(),
+      order: z.number(),
     }),
 });
 
--- src/content/services/ai-solutions.md
+++
@@ -1,7 +0,0 @@
----
-heading: AI Solutions
-image: ../../assets/svgs/AI-solutions.svg
-order: 1
----
-
-Offload repetitive workflows with intelligent automation built to scale with your team.
